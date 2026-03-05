d543f2c2cb38ff03377d8281b2accceddb775a20
Osvaldo Angel Hernandez Gutierrez <osvaldoangelhernandezgutierrez@Mac-mini-de-Osvaldo.local>
2026-03-05 10:25:12 -0600

    primer commit

---
+++ lib/common/settings/routes_names.dart
@@ -0,0 +1,17 @@
+class RoutesNames {
+  static const String welcomePage = '/';
+  static const String homePage = '/homePage';
+  static const String loginPage = '/login';
+  static const String registerPage = '/register';
+  static const String foryoupage = '/forYoupage';
+  static const String nearbyProfilesPage = '/nearbyProfiles';
+  static const String profileDetailPage = '/profileDetail';
+  static const String preferencesPage = '/preferences';
+  static const String radarScannerPage = '/radarScanner';
+  static const String chatPage = '/chatPage';
+  static const String startConversationsPage = '/start-conversations-page';
+  static const String userProfileDetailPage = '/user-profile-detail-page';
+  static const String blockedUsersPage = '/blocked-users-page';
+  static const String updateProfilePage = '/update-profile-page';
+  static const String tutorialPage = '/tutorial-page';
+}
